02f4b6ed4e2b5d2cd878cb34384e7705e5cd8bdf
Harmi Sutariya <sutariyaharmi@gmail.com>
2024-06-27 10:45:00 +0530

    27/6/24

---
+++ My_Sql/Lecture/lecture1.sql
@@ -0,0 +1,22 @@
+=> connect mysql
+-- \sql
+--\connect root@localhost:3306
+
+=>show all databases command :
+-- show databases;
+
+=> create database command:
+-- create database <databasename>;
+
+=> use perticular database command:
+-- use <databasename>
+
+=> show perticular database tables :
+--show tables;
+
+=> create new table :
+--create table user( firstname varchar(30) , lastname varchar(30) , age int);
+
+=> describe or show perticular table (row and column);
+-- desc user;
+-- describe user;
